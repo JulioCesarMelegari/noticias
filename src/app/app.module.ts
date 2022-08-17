dccb3da0e533bff5e163e240d77b0f976cf48efa
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2022-08-17 09:57:15 -0300

    projeto inicial, bootstrap, rotas, componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NoticiasModule } from './noticias/noticias.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NoticiasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
